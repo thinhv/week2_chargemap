f9b4d1767510770bca9445bb98bc87a3652b818a
Thinh Vo <ducthinh2410@gmail.com>
2020-03-29 23:06:33 +0300

    Fix station's location point

--- models/station.js
+++ models/station.js
@@ -2,13 +2,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+require('./connection')
+require('./connectionType')
+require('./currentType')
+require('./level')
+
+const pointSchema = new mongoose.Schema({
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  })
+
 const stationSchema = new Schema({
     Title: String,
     Town: String,
     AddressLine1: String,
     StateOrProvince: String,
     Postcode: String,
-    Location: { type: String, coordinates: [Number] },
+    Location: pointSchema,
     Connections: [{ type: Schema.Types.ObjectId, ref: 'Connection' }]
 })
 
